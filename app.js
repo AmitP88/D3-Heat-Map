abaeb533b94c4efa108d9b4b0ab08107fa255340
Amit Patel <amitpatel_7@msn.com>
2018-09-09 14:23:12 -0700

    created x and y scales and added svg canvas

--- app.js
+++ app.js
@@ -2,5 +2,35 @@ const getData = () => {
     d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json", (error, dataset) => {
         if (error) console.log(error);
         console.log(dataset);
+
+        const w = 1000;
+        const h = 500;
+
+        /* Padding between SVG canvas boundary and the plotted data */
+        const padding = 40;
+
+        /* Scale for x-axis */
+        let x_min = d3.min(dataset.monthlyVariance, (d) => d.year);
+        let x_max = d3.max(dataset.monthlyVariance, (d) => d.year);
+        const xScale = d3.scaleTime()
+                         .domain([x_min, x_max])
+                         .range([padding, w - padding]);
+
+        /* Scale for y-axis */
+        let y_min = d3.min(dataset.monthlyVariance, (d) => d.month);
+        let y_max = d3.max(dataset.monthlyVariance, (d) => d.month);
+        const yScale = d3.scaleTime()
+                         .domain([y_min, y_max])
+                         .range([h - padding, padding]);
+
+        /* Add an SVG Canvas */
+        const svg = d3.select(".container")
+                      .append("svg")
+                      .attr("class", "canvas");
+
+
+
+
+
     });
 }
